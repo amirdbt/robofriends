09f4a357d4a2366aeb28eca03c8182c793e11ce1
amirdbt <hassanamir643@yahoo.com>
2019-09-05 00:45:14 -0700

    Updates

---
+++ precache-manifest.6dcbe4ab58c3c3f39e5c989663b48298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217b2840eb66b5ea06dcf34e7792edfe",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5cd12b4ab43d0295ca7d",
+    "url": "/robofriends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "d1329bd86b33220361ca",
+    "url": "/robofriends/static/css/main.63d6df8d.chunk.css"
+  },
+  {
+    "revision": "5cd12b4ab43d0295ca7d",
+    "url": "/robofriends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "d1329bd86b33220361ca",
+    "url": "/robofriends/static/js/main.8734e496.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
