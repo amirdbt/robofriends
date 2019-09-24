baf2b8b90128d3742bc9206298eb6436b44eaa73
amirdbt <hassanamir643@yahoo.com>
2019-09-24 15:28:34 +0100

    added thunk redux to the project and added a reducer for the fetch api

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,58 +2,61 @@ import React from "react"
 import CardList from "../components/CardList"
 import SearchBox from "../components/SearchBox"
 import Scroll from "../components/Scroll"
-import { setSearchField } from "../actions"
+import { setSearchField, requestRobots } from "../actions"
 import { connect } from "react-redux"
 import "./App.css"
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      robots: []
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     robots: []
+  //   }
+  // }
   // onSearchChange = event => {
   //   this.setState({
   //     searchfield: event.target.value
   //   })
   // }
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(response => {
-        this.setState({
-          robots: response
-        })
-      })
+    this.props.onRequestRobots()
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     this.setState({
+    //       robots: response
+    //     })
+    //   })
   }
   render() {
-    const { searchField, onSearchChange } = this.props
-    const filteredRobots = this.state.robots.filter(robot => {
+    const { searchField, onSearchChange, robots, isPending } = this.props
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={onSearchChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      )
-    }
+    return isPending ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    )
   }
 }
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobot.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 const mapDispatchToProps = dispatch => ({
-  onSearchChange: event => dispatch(setSearchField(event.target.value))
+  onSearchChange: event => dispatch(setSearchField(event.target.value)),
+  onRequestRobots: () => dispatch(requestRobots())
 })
 export default connect(
   mapStateToProps,
